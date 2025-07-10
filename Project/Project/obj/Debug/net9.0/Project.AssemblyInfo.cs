f66db2305575661a99a7a0ff001de6fe8817e6a4
Ali <ali.kh841410@gmail.com>
2025-07-10 23:20:51 +0330

    fix: problem of setting instructor to database

--- Project/Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ Project/Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1660c79c9e2792538bb6d65e688e340784066827")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d41bbf8305b2c4e4445160e7b073ad27f2073d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
