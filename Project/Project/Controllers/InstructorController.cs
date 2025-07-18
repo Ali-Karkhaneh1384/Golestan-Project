b620106a55a32e17e1b6818562d4c66251d7003a
Ali <ali.kh841410@gmail.com>
2025-07-18 13:49:35 +0330

    problem: problem of submiting grade

--- Project/Project/Controllers/InstructorController.cs
+++ Project/Project/Controllers/InstructorController.cs
@@ -1,6 +1,7 @@
 ﻿using Golestan_Project.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Project.Models;
 using System.Threading.Tasks;
 
 namespace Project.Controllers
@@ -63,6 +64,40 @@ namespace Project.Controllers
             return View(section);
                 
         }
+        //[HttpPost]
+        //public async Task<IActionResult> SubmitGrade([Bind("student_id, section_id, grade")] takes model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return RedirectToAction("ShowSectionDetails", new { sectionId = model.section_id, error = "داده‌های ورودی معتبر نیستند." });
+        //    }
+
+        //    var selectedTakes= _dbContext.takes
+        //        .FirstOrDefault(t => t.section_id == model.section_id && t.student_id == model.student_id);
+
+        //    if (selectedTakes == null)
+        //    {
+        //        return RedirectToAction("ShowSectionDetails", new { sectionId = model.section_id, error = "رابطه دانشجو با این بخش یافت نشد." });
+        //    }
+
+        //    if (model.grade >= 0 && model.grade <= 20)
+        //    {
+        //        selectedTakes.grade = model.grade;
+        //        try
+        //        {
+        //            await _dbContext.SaveChangesAsync();
+        //            return RedirectToAction("ShowSectionDetails", new { sectionId = model.section_id, success = "نمره با موفقیت ثبت شد." });
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            return RedirectToAction("ShowSectionDetails", new { sectionId = model.section_id, error = "خطا در ثبت نمره: " + ex.Message });
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("ShowSectionDetails", new { sectionId = model.section_id, error = "نمره باید بین 0 تا 20 باشد." });
+        //    }
+        //}
 
     }
 }
