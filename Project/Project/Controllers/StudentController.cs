23365c20f5509b6990dff77c40d934f780e5cf7c
Ali <ali.kh841410@gmail.com>
2025-07-19 12:59:43 +0330

    feat: grades of student and section details

--- Project/Project/Controllers/StudentController.cs
+++ Project/Project/Controllers/StudentController.cs
@@ -37,5 +37,49 @@ namespace Project.Controllers
             return View(student);
          
         }
+        public IActionResult ShowSectionDetails(int sectionId)
+        {
+            var section = _dbContext.sections
+                .Include(s => s.classroom)
+                .ThenInclude(c => c.sections)
+                .ThenInclude(s => s.course)
+                .ThenInclude(c => c.sections)
+                .ThenInclude(s => s.section_Times)
+                .ThenInclude(s => s.time_slot)
+                .ThenInclude(s => s.section_Times)
+                .ThenInclude(s => s.section)
+                .ThenInclude(s => s.teach)
+                .ThenInclude(t => t.instructor)
+                .ThenInclude(i => i.user)
+                .Include(s => s.takes)
+                .ThenInclude(t => t.students)
+                .ThenInclude(s => s.users)
+                .FirstOrDefault(s => s.Id == sectionId);
+            if (section == null)
+            {
+                return NotFound("کلاس درس یافت نشد");
+            }
+            return View(section);
+
+        }
+        public IActionResult Grades()
+        {
+            int? studentId = HttpContext.Session.GetInt32("StudentId");
+            var student = _dbContext.students
+                .Include(s => s.takes)
+                .ThenInclude(t => t.sections)
+                .ThenInclude(s => s.course)
+                .ThenInclude(c => c.sections)
+                .ThenInclude(s => s.teach)
+                .ThenInclude(t => t.instructor)
+                .ThenInclude(i => i.user)
+                .FirstOrDefault(s => s.student_id == studentId);
+            if(student == null)
+            {
+                return NotFound("نمره ای یافت نشد");
+            }
+            return View(student);
+        }
+        
     }
 }
